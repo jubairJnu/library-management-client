c198977ecfa8e3b991bde2297a9da3e6213327f4
Abdullah Al Jubair <jubair2810@gmail.com>
2026-01-15 23:45:58 +0600

    remove counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { useCounterStore } from "@/providers/counter-store-provider";
-import React from "react";
-
 const HomePage = () => {
-  const { count, incrementCount, decrementCount } = useCounterStore(
-    (state) => state
-  );
-  return (
-    <div className="flex h-screen items-center justify-center gap-3.5">
-      <Button onClick={incrementCount} className="bg-blue-500">
-        +
-      </Button>
-      <p className="text-2xl text-red-500 px-5 py-3 border rounded-lg ">
-        {count}
-      </p>
-      <Button onClick={decrementCount} className="bg-red-500">
-        {" "}
-        -{" "}
-      </Button>
-    </div>
-  );
+  return <h1>Home page</h1>;
 };
 
 export default HomePage;
